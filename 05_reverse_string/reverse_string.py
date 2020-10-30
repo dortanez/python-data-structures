fd35174fc67406a845656e089721a36839eeaf0c
Danisa Ortanez <dortanez@yahoo.com>
2020-10-30 14:04:40 -0700

    python

--- 05_reverse_string/reverse_string.py
+++ 05_reverse_string/reverse_string.py
@@ -1,4 +1,13 @@
 def reverse_string(phrase):
+    return phrase[::-1]
+    # arr = []
+    # str = ''
+    # for char in phrase:
+    #     arr.append(char)
+    # for letter in phrase:
+    #     str += arr.pop()
+    # return str
+    
     """Reverse string,
 
         >>> reverse_string('awesome')
