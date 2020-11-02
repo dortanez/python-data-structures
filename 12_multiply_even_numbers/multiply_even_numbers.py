3263e695ee1cb61b973c2507b123ca0e60a2fc66
Danisa Ortanez <dortanez@yahoo.com>
2020-11-01 20:40:44 -0800

    python

--- 12_multiply_even_numbers/multiply_even_numbers.py
+++ 12_multiply_even_numbers/multiply_even_numbers.py
@@ -1,4 +1,9 @@
 def multiply_even_numbers(nums):
+    result = 1
+    for num in nums:
+        if num % 2 == 0:
+            result *= num
+    return result
     """Multiply the even numbers.
     
         >>> multiply_even_numbers([2, 3, 4, 5, 6])
