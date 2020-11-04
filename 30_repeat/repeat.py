226522710a89d3f94281316e4dff1110a11b5db7
Danisa Ortanez <dortanez@yahoo.com>
2020-11-04 07:31:59 -0800

    python

--- 30_repeat/repeat.py
+++ 30_repeat/repeat.py
@@ -1,4 +1,10 @@
 def repeat(phrase, num):
+    if type(num) is not int or num < 0:
+        return None
+    string = ''
+    for n in range(0,num):
+        string += phrase
+    return string
     """Return phrase, repeated num times.
 
         >>> repeat('*', 3)
